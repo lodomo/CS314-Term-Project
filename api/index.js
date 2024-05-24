3985a6edfb748b08c661ca8977b6f0d4edd24395
Lorenzo Moon <lhaskins@cecs.pdx.edu>
2024-05-23 23:31:33 -0700

    Users can register and login

--- api/index.js
+++ api/index.js
@@ -1,37 +1,59 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 
+const mongoose = require("mongoose");
+const User = require("./models/User");
+
 dotenv.config();
+
 const MONGO_URI = process.env.MONGO_URI;
 const CLIENT_URL = process.env.CLIENT_URL;
 
 mongoose.connect(MONGO_URI);
 
 // Use CORS middleware to allow requests from http://localhost:5173
-app.use(cors({
-  origin: CLIENT_URL 
-}));
+app.use(
+    cors({
+        origin: CLIENT_URL,
+    }),
+);
 
 // Use body-parser middleware to parse JSON requests
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json('You do not belong here');
+app.get("/", (req, res) => {
+    res.json("You do not belong here");
 });
 
-app.post('/api/login', (req, res) => {
-  const { token, name, email, picture } = req.body;
+app.post("/api/login", async (req, res) => {
+    const { name, email, picture } = req.body;
+    let status_message = "None";
 
-  // Process the user data as needed
-  console.log('Received user data:', { token, name, email, picture });
+    try {
+        let user = await User.findOne({ email });
 
-  // Respond back to the client
-  res.json({ message: 'User data received', user: { name, email, picture } });
+        if (!user) {
+            user = new User({ name, email, picture });
+            await user.save();
+            
+            status_message = "New user created";
+        } else {
+            status_message = "User Logged In";
+        }
+
+        console.log(status_message, ":", user);
+
+        res.json({
+            message: status_message,
+            user: { name, email, picture },
+        });
+    } catch (error) {
+        console.error("Error processing user data:", error);
+        res.status(500).json({ error: "Internal Server Error" });
+    }
 });
 
 app.listen(4000);
